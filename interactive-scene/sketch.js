31d757032d7c253d4f9530afeb9e50ccf7bf85c5
6mnyb2wtxp-code <6mnyb2wtxp@privaterelay.appleid.com>
2026-03-02 18:44:10 -0500

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -8,9 +8,45 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  background(220);
 }
 
 function draw() {
-  background(100);
-  circle(mouseX, mouseY, 50);
+  mouseClicked();
+}
+
+
+
+function mouseClicked(){
+  drawRect();
+  drawCircle();
+  blackScreen();
+  whiteScreen ();
+} 
+
+function drawRect() {
+  if (keyIsDown(82)) {
+    fill("red");
+    rect(mouseX, mouseY, 100, 50);
+  }
+}
+
+function drawCircle() {
+  if (keyIsDown(67)) {
+    fill("blue");
+    circle(mouseX, mouseY, 100);
+  }
+}
+
+function blackScreen() {
+  if (keyIsDown(66)) {
+    background(0);
+  }
+}
+
+function whiteScreen () {
+  if (keyIsDown(87)) {
+    background(255)
+  }
 }
