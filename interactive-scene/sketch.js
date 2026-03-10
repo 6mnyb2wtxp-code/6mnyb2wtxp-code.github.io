8c20b0bbf6b62f24fc9121fb0df9aa9f5a3ad15c
6mnyb2wtxp-code <6mnyb2wtxp@privaterelay.appleid.com>
2026-03-10 11:02:31 -0600

    massive not working update

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -6,15 +6,35 @@
 // - describe what you did to take this project "above and beyond"
 
 let Color = (25, 25, 100);
-let shapenum = 0 ;
+let shapeNum = 0 ;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  background(220);
+  background(0);
 }
 
 function draw() {
-  text(`The selected shape is  ${shapenum}` , 75, 75);
+  //display what shape is selected
+  chooseShape();
+  textSize(32);
+  fill("white");
+  text(`The selected shape is  ${shapeNum}` , 75, 75);
 }
 
+function chooseShape() {
+  if (keyIsDown(82)) {
+    shapeNum = 1;
+  }
+  else if (keyIsDown(67)) {
+    shapeNum = 2;
+  }
+}
+
+// put in function 
+if (mouseIsPressed) {
+  if (shapeNum === 1) {
+    fill(Color);
+    rect(mouseX, mouseY, 100, 50);
+  }
+}
