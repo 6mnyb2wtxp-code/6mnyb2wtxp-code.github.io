9f8f515b7fa462e7286d35a49db701a77c152344
6mnyb2wtxp-code <6mnyb2wtxp@privaterelay.appleid.com>
2026-03-17 11:03:22 -0600

    fixed bugs

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -4,19 +4,20 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let X = width/2;
-let Y = length/2;
-playerSize = 50;
+
+
+let x = 200; 
+let y = 200;
+let playerSize = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-};
+}
 
 function draw() {
   background(0);
 
-  
-  fill(255);
-  circle(X, Y, playerSize);
+  fill("white");
+  circle(x, y, playerSize);
   
 }
